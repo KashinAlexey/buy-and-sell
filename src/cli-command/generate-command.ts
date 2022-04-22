23512806677edd8eade975c977d130c2a679e0ab
Alexey <apple230379@gmail.com>
2022-04-22 21:23:47 +0300

    2.16. Рефакторит команду GenerateCommand. Воспользуемся в команде  классом .

--- src/cli-command/generate-command.ts
+++ src/cli-command/generate-command.ts
@@ -1,4 +1,4 @@
-import { appendFile } from 'fs/promises';
+import TSVFileWriter from '../common/file-writer/tsv-file-writer.js';
 import got from 'got';
 import OfferGenerator from '../common/offer-generator/offer-generator.js';
 import { MockData } from '../types/mock-data.type.js';
@@ -19,9 +19,10 @@ export default class GenerateCommand implements CliCommandInterface {
     }
 
     const offerGeneratorString = new OfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
 
     for (let i = 0; i < offerCount; i++) {
-      await appendFile(filepath, `${offerGeneratorString.generate()}\n`, 'utf8');
+      await tsvFileWriter.write(offerGeneratorString.generate());
     }
 
     console.log(`File ${filepath} was created!`);
