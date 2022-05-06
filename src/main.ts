c48ca87a8d56be42b9dcfa7ee1fd7fc9616040b7
Alexey <apple230379@gmail.com>
2022-05-06 17:18:07 +0300

    4.22. Добавит внедрение зависимостей в UserService. WIP Воспользуемся декоратором  и внедрим в конструктор в качестве зависимости . После этого внутри класса мы сможем залогировать событие через .
    Последним шагом добавим для класса  декоратор .
    Этим классом мы будем пользоваться из разных мест приложения,
    поэтому сразу поместим его в контейнер. Для этого откроем объект
     и добавим ещё один компонент — .
    А затем в  добавим для него привязку.
    
    WIP: Имплементирован только один метод интерфейса .
    Линтер ругается.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {ConfigInterface} from './common/config/config.interface.js';
 import ConfigService from './common/config/config.service.js';
 import {DatabaseInterface} from './common/database-client/database.interface.js';
 import DatabaseService from './common/database-client/database.service.js';
+import {UserServiceInterface} from './modules/user/user-service.interface.js';
+import UserService from './modules/user/user.service.js';
 import Application from './app/application.js';
 
 const applicationContainer = new Container();
@@ -13,6 +15,7 @@ applicationContainer.bind<Application>(Component.Application).to(Application).in
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
+applicationContainer.bind<UserServiceInterface>(Component.UserServiceInterface).to(UserService);
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
