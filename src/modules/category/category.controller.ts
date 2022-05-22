1ed9a2bf7d79f2ad67efa04dbbd0f1599bd94c56
Alexey <apple230379@gmail.com>
2022-05-22 17:29:17 +0300

    5.19. Добавит create в CategoryController. Перед созданием категории выполним проверку на существование категории. Если категория существует, то вернём соответствующий код состояния — `422`. В противном случае добавим новую категорию и вернём её клиенту.
    Данные для создания категория возьмём из тела запроса. Для этого
    обратимся к полю `body` объекта `Request`. Мы воспользовались
    middleware `express.json()`, поэтому в `body` будет обычный объект.
    
     Для проверки обработчика создадим тестовый запрос.

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -7,9 +7,9 @@ import {LoggerInterface} from '../../common/logger/logger.interface.js';
 import {HttpMethod} from '../../types/http-method.enum.js';
 import {CategoryServiceInterface} from './category-service.interface.js';
 import {StatusCodes} from 'http-status-codes';
-import {plainToInstance} from 'class-transformer';
 import CategoryDto from './dto/category.dto.js';
 import {fillDTO} from '../../utils/common.js';
+import CreateCategoryDto from './dto/create-category.dto.js';
 
 @injectable()
 export default class CategoryController extends Controller {
@@ -30,7 +30,23 @@ export default class CategoryController extends Controller {
     this.send(res, StatusCodes.OK, categoriesDTO);
   }
 
-  public create(_req: Request, _res: Response): void {
-    // Код обработчика
+  public async create(
+    {body}: Request<Record<string, unknown>, Record<string, unknown>, CreateCategoryDto>,
+    res: Response): Promise<void> {
+
+    const existCategory = await this.categoryService.findByCategoryName(body.name);
+
+    if (existCategory) {
+      const errorMessage = `Category with name «${body.name}» exists.`;
+      this.send(res, StatusCodes.UNPROCESSABLE_ENTITY, {error: errorMessage});
+      return this.logger.error(errorMessage);
+    }
+
+    const result = await this.categoryService.create(body);
+    this.send(
+      res,
+      StatusCodes.CREATED,
+      fillDTO(CategoryDto, result)
+    );
   }
 }
